ce00706f4b4059ea936661b1452583dc4a59b3c2
Charles <chall70101@gmail.com>
2021-07-05 10:40:51 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+
+function writeCards(names, event){
+   const greeting = []
+    for(let i = 0; i < names.length; i++){
+        console.log(greeting.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!`   ));
+    }
+    return greeting
+}
+
+
+
+function countDown(num){
+    
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
+
